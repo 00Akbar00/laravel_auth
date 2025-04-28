36f495299d5bae3806ac5cebb4be6c04516857f0
Muhammad Akbar <muhammadakbar@Muhammad-Akbar.local>
2025-04-28 10:36:08 +0500

    some files added and some deleted

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
-// Authentication Routes
-Route::middleware('guest')->group(function () {
-    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-    Route::post('/register', [AuthController::class, 'register']);
-    
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
-    
-    Route::get('/forgot-password', [AuthController::class, 'showForgotPasswordForm'])->name('password.forgot');
-    Route::post('/send-otp', [AuthController::class, 'sendPasswordResetOtp'])->name('password.otp.send');
-    
-    Route::get('/reset-password', [AuthController::class, 'showResetPasswordForm'])->name('password.reset.form');
-    Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
-});
 
-// Email Verification
-Route::get('/verify-email/{token}', [AuthController::class, 'verifyEmail'])->name('verify.email');
-
-// Logout
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
-
-// Home
 Route::get('/', function () {
-    return view('home');
-})->middleware('auth');
+    return view('welcome');
+});
